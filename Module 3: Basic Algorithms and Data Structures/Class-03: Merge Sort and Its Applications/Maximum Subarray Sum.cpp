d14750da2165d88e2029876fefc1a9419767341e
Md Aminul Islam Amin <157211474+AmiNuL-72@users.noreply.github.com>
2026-01-27 12:32:04 +0600

    Add files via upload

---
+++ Module 3: Basic Algorithms and Data Structures/Class-03: Merge Sort and Its Applications/Maximum Subarray Sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  long long
+#define pb  push_back
+#define all(x) (x).begin(), (x).end()
+#define  gcd(a,b)  __gcd(a,b)
+#define    lcm(a,b)  (a*b)/__gcd(a,b)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solve(){
+    
+}
+
+signed main()
+{
+    optimize();
+    ll t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
